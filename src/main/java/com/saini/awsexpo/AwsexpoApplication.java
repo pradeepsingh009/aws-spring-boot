16a1599de1b9010b39a6d0526cc513ff7525ee62
spradeepsinghsaini@gmail.com <spradeepsinghsaini@gmail.com>
2020-02-09 14:08:36 +0530

    first commit empty project with indexConroller

---
+++ src/main/java/com/saini/awsexpo/AwsexpoApplication.java
@@ -0,0 +1,13 @@
+package com.saini.awsexpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsexpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsexpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saini/awsexpo/AwsexpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saini.awsexpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsexpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
