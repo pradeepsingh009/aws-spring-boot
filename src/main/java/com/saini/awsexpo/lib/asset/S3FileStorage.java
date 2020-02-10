02d985aa8d34bccf83c8e99fef3946266ec9b88a
Pradeep Singh <pradeep.singh@happiestminds.com>
2020-02-10 20:11:36 +0530

    putobject request created

--- src/main/java/com/saini/awsexpo/lib/asset/S3FileStorage.java
+++ src/main/java/com/saini/awsexpo/lib/asset/S3FileStorage.java
@@ -2,14 +2,20 @@ package com.saini.awsexpo.lib.asset;
 
 import java.io.File;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
 import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 public class S3FileStorage implements FileStorage {
 	
 	private AmazonS3 s3client;
+	
+	@Value("${aws.s3.bucket.name}")
+	private String bucketName;
 	
 	public S3FileStorage() {
 		s3client = AmazonS3ClientBuilder.standard().withCredentials(new EnvironmentVariableCredentialsProvider()).withRegion(Regions.US_EAST_1).build();
@@ -18,6 +24,7 @@ public class S3FileStorage implements FileStorage {
 
 	@Override
 	public boolean create(File file,String key) {
+		PutObjectRequest request = new PutObjectRequest(this.bucketName,key,file);
 		// TODO Auto-generated method stub
 		return false;
 	}
